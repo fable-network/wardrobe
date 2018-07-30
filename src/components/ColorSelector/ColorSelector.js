c6b9576e4a644c39152092582313eb7a0a04f861
zoheiry <ali.elzoheiry@gmail.com>
2018-07-30 16:09:37 +0200

    Finalize color selector compoentn

--- src/components/ColorSelector/ColorSelector.js
+++ src/components/ColorSelector/ColorSelector.js
@@ -8,40 +8,61 @@ const pulse = keyframes`
   100% { transform: scale(1); }
 `;
 
-const ColorWrapper = styled.span`
+const ColorWrapper = styled('span')`
   background-color: #fff;
   border-radius: 100%;
   box-shadow: 0px 0px 0px 1px #cecece;
   border: 2px solid #FFF;
   cursor: ${props => (props.isStatic ? 'default' : 'pointer')};
-  transform: ${props => (props.isStatic ? 'scale(1);' : 'scale(.8);')};
+  transform: ${props => (props.isStatic || props.selected ? 'scale(1);' : 'scale(.8);')};
   display: inline-block;
   vertical-align: middle;
   margin: 0 4px 0 2px;
   overflow: hidden;
   padding: 1em;
   transition: all .3s ease;
-  background: ${props => props.background};
+  background: ${props => (props.patternImage
+    ? `url(${props.patternImage}) no-repeat 50% 50%`
+    : props.color
+  )};
   &:hover {
     transform: scale(1);
   }
 `;
 
-const ColorSelector = ({ onClick, patternImage, color }) => (
-  <ColorWrapper
-    isStatic={!onClick} // maybe not have this handled by the click listener?
-    background={patternImage || color}
-    onClick={onClick}
-  />
+const TextWrapper = styled('span')`
+  vertical-align: middle;
+  margin-left: 2px;
+  font-weight: ${props => (props.selected ? 500 : 400)};
+`;
+
+const ColorSelector = ({ onClick, patternImage, color, isStatic, text, selected }) => (
+  <div>
+    <ColorWrapper
+      isStatic={isStatic}
+      patternImage={patternImage}
+      color={color}
+      onClick={!isStatic ? onClick : null}
+      selected={selected}
+    />
+    {text && <TextWrapper selected={selected}>{text}</TextWrapper>}
+  </div>
 );
+
+ColorSelector.defaultProps = {
+  color: '#FFF',
+  selected: false,
+  isStatic: false
+}
 
 ColorSelector.propTypes = {
   color: PropTypes.string,
-  patternImage: PropTypes.string //overrides color if available
+  patternImage: PropTypes.string, //overrides color if available
+  onClick: PropTypes.func,
+  isStatic: PropTypes.bool, // ignores click handler if true
+  text: PropTypes.string,
+  selected: PropTypes.bool 
 };
 
-ColorSelector.defaultProps = {
-
-}
 
 export default ColorSelector;
--- src/components/ColorSelector/ColorSelector.md
+++ src/components/ColorSelector/ColorSelector.md
@@ -1,8 +1,9 @@
-Color Selector:
+Static color selector:
 
 ```jsx
 <ColorSelector
-  color="red"
+  color="#e25454"
+  isStatic={true}
 />
 ```
 
@@ -10,7 +11,34 @@ Color Selector with click handler:
 
 ```jsx
 <ColorSelector
-  color="blue"
+  color="#5F9DC7"
   onClick={() => alert('Clicked!')}
+/>
+```
+
+Color selector with pattern image:
+
+```jsx
+<ColorSelector
+  patternImage="https://app.fashiontrade.construction/api/image/be8eb3d59eabe9dcaa41383eba5ba43bc641e7a9980ed4af02714401871a314a.jpg?w=265&h=330"
+/>
+```
+
+Color Selector with text:
+
+```jsx
+<ColorSelector
+  color="#aecc76"
+  text="Lime green"
+/>
+```
+
+Selected color selector:
+
+```jsx
+<ColorSelector
+  color="#aecc76"
+  selected={true}
+  text="Lime green"
 />
 ```
