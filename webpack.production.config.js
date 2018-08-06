10733911bea2b9284573e99153f69f998b8910f5
zoheiry <ali.elzoheiry@gmail.com>
2018-08-06 15:54:11 +0200

    Change rule

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -12,7 +12,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.js?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
