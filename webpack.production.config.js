b803e5f5751d8feb03689de7122ae54aafa44ecb
zoheiry <ali.elzoheiry@gmail.com>
2018-08-06 15:39:10 +0200

    Add webpack build to production

---
+++ webpack.production.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, './build/lib'),
+    filename: 'index.js',
+    library: '',
+    libraryTarget: 'commonjs'
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader?modules&importLoaders=1',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      },
+    ],
+  }
+};
