ea7771a5f08455fc54779ba792084697d0b5ffa3
zoheiry <ali.elzoheiry@gmail.com>
2018-08-06 11:42:44 +0200

    Cleanup test generation script

--- generateTests.js
+++ generateTests.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-let tests = [];
 
 const componentNames = [
   'Badge',
@@ -15,51 +14,11 @@ import ~COMPONENT_NAME~ from './~COMPONENT_NAME~';
 describe('~COMPONENT_NAME~ Component', () => {
   describe('snapshots', () => {
     it('should match snapshot', () => {
-      ~YIELD~
+    ~YIELD~
     });
   });
 });
 `
-
-const finishedGeneration = (progress) =>
-  Object.keys(progress).filter(key => progress[key] === false).length === 0
-
-const progress = componentNames.reduce((acc, name) => acc[name] = false,{});
-
-componentNames.forEach(name => {
-  fs.readFile(`./src/components/${name}/${name}.md`, (err, out) => {
-    if (err) {
-      throw (err);
-    }
-
-    const file = out.toString();
-
-    const regexString =  `\`\`\`jsx\\n<${name}[\\s\\S]+?(\\/>|<\\/${name}>)`;
-    // ```jsx\n (starts matching when it finds a code block that begins with this string)
-    // <${name} (Ensures that the string begins with the component we are currently on)
-    // [\s\S]+? (matches any white space character as few times as possible)
-    // (\/>|<\/${name}>) untill it reaches "/>" or "</ComponentName>"
-
-    const regex = new RegExp(regexString, 'g')
-    const matches = file.match(regex);
-    // removes the extra characters from the matched strings (jsx & ```).
-    const components = matches.map(match => match.replace(/```/g, '').replace(/jsx/g, ''));
-    const snapshotCode = components.map((component, i) => (
-      `
-      const wrapper${i + 1} = shallow(${component.replace(/\n/g, '')});
-      expect(wrapper${i + 1}).toMatchSnapshot();
-      `
-    )).join('');
-
-    const result = boilerPlate.replace('~YIELD~', snapshotCode).replace(/~COMPONENT_NAME~/g, name);
-    tests.push({ name: name, code: result });
-
-    progress[name] = true;
-    if (finishedGeneration(progress)) {
-      writeTests(tests);
-    }
-  });
-});
 
 const writeTests = (tests) => {
   tests.forEach(test => {
@@ -70,3 +29,64 @@ const writeTests = (tests) => {
     });
   })
 }
+
+const finishedGeneration = (progress) =>
+  Object.keys(progress).filter(key => progress[key] === false).length === 0;
+
+const injectSnapshotCode = (code, componentName) =>
+  boilerPlate.replace('~YIELD~', code).replace(/~COMPONENT_NAME~/g, componentName);
+
+// removes the extra characters from the matched strings (jsx & ```).
+const cleanComponentCode = (code) =>
+  code.map(component => component.replace(/```/g, '').replace(/jsx/g, ''));
+
+const readMarkdownFile = (fileName, callBack) => {
+  fs.readFile(`./src/components/${fileName}/${fileName}.md`, (err, out) => {
+    if (err) {
+      throw (err);
+    } else {
+      const file = out.toString();
+      callBack(file);
+    }
+  });
+};
+
+const getMatchingComponents = (file, componentName) => {
+  const regexString =  `\`\`\`jsx\\n<${componentName}[\\s\\S]+?(\\/>|<\\/${componentName}>)`;
+  // ```jsx\n (starts matching when it finds a code block that begins with this string)
+  // <${componentName} (Ensures that the string begins with the component we are currently on)
+  // [\s\S]+? (matches any white space character as few times as possible)
+  // (\/>|<\/${componentName}>) untill it reaches "/>" or "</ComponentName>"
+
+  const regex = new RegExp(regexString, 'g')
+  const matches = file.match(regex);
+  const components = cleanComponentCode(matches);
+
+  return components;
+}
+
+const generateTests = () => {
+  const tests = [];
+  const progress = componentNames.reduce((acc, name) => acc[name] = false,{});
+  componentNames.forEach(name => {
+    readMarkdownFile(name, (file) => {
+      const components = getMatchingComponents(file, name);
+      const snapshotCode = components.map((component, i) => (
+        `
+      const wrapper${i + 1} = shallow(${component.replace(/\n/g, '')});
+      expect(wrapper${i + 1}).toMatchSnapshot();
+        `
+      )).join('');
+
+      const result = injectSnapshotCode(snapshotCode, name);
+      tests.push({ name: name, code: result });
+
+      progress[name] = true;
+      if (finishedGeneration(progress)) {
+        writeTests(tests);
+      }
+    });
+  });
+}
+
+generateTests();
