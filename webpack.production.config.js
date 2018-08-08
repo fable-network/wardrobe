0a135f0584f3f9097ee60f9e631fab1a5bb39637
zoheiry <ali.elzoheiry@gmail.com>
2018-08-08 11:03:19 +0200

    publish the package under fashiontrade scope

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'index.js',
-    library: 'wardrobe',
+    library: '',
     libraryTarget: 'commonjs2'
   },
   module: {
