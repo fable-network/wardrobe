862567251af574606935d0603943f85da6083f80
frontalicious-martijn <martijn@frontalicious.nl>
2018-08-09 13:02:57 +0200

    FC-362 Added Icon documentation.

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Icon = ({ name, color, width, height }) => (
-  <svg className="ft-icon" fill={color} width={width} height={height}>
+/**
+ * Uses a SVG sprite to show icons based on name
+ */
+const Icon = ({ name, color, width, height, ...otherProps }) => (
+  <svg className="ft-icon" fill={color} width={width} height={height} {...otherProps}>
     <use xlinkHref={`/iconsprite.svg#icon-${name}`} />
   </svg>
 );
 
 Icon.propTypes = {
+  /** Name of the icon as defined in the sprite */
   name: PropTypes.string.isRequired,
+  /** Color for the SVG `fill`  attributes */
   color: PropTypes.string,
+  /** Change width; SVG ratio will be preserved */
   width: PropTypes.string,
+  /** Change height; SVG ratio will be preserved */
   height: PropTypes.string,
 };
 
