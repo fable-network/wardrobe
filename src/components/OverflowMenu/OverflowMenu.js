b1cea27926a20008632b5454ef2b53d3a193f988
zoheiry <ali.elzoheiry@gmail.com>
2018-08-14 14:05:50 +0200

    add overflow menu component

---
+++ src/components/OverflowMenu/OverflowMenu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './OverflowMenu.scss';
+
+const OverflowMenu = (props) => (
+
+);
+
+OverflowMenu.defaultProps = {};
+
+OverflowMenu.propTypes = {};
+
+export default OverflowMenu;
