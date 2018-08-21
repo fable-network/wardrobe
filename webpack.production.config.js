4ea9de4a887dd4fdf9f206ef70ad62aeeec1fbce
zoheiry <ali.elzoheiry@gmail.com>
2018-08-21 11:27:16 +0200

    Use relative path for webpack entry point

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/components/index.js'),
+  entry: path.resolve(__dirname, './src/components/index.js'),
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'index.js',
