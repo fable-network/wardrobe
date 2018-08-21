379e556a3e4d8a31c4997b73c9eb70c48e8bbbf2
zoheiry <ali.elzoheiry@gmail.com>
2018-08-21 11:32:13 +0200

    Undo changes to library property

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'index.js',
-    library: '@fashiontrade/wardrobe',
+    library: '',
     libraryTarget: 'commonjs'
   },
   module: {
