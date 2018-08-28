758f35e6471ff2599c605b240b000e4a1a0262c8
zoheiry <ali.elzoheiry@gmail.com>
2018-08-28 13:06:34 +0200

    add back svg inline loader

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -21,7 +21,10 @@ module.exports = {
         test: /\.css$/,
         loader: 'style-loader!css-loader?modules&importLoaders=1',
       },
-      
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader',
+      },
       {
         test: /\.scss$/,
         use: [
