8354048a39157a3620a029f665eb2b24ea3c4a61
zoheiry <ali.elzoheiry@gmail.com>
2018-08-29 15:43:38 +0200

    Intial setup of searchbar

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,28 +1,75 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { withTheme } from 'styled-components';
+
+import searchIcon from './searchIcon'; // TODO find a way to use Icon component correctly
 
 const Wrapper = styled('div')`
   position: relative;
   background: #FFF;
   color: ${props => props.theme.ravenBlack};
-  border: ${props => props.theme.stoneGrey};
+  display: flex;
+  align-items: center;
+  color: inherit;
 `;
 
-const Searchbar = (props) => (
-  <Wrapper />
+const Input = styled('input')`
+  width: 100%;
+  padding: 10px;
+  ${props => `padding-${props.iconPosition}: 40px;`}
+  border: solid 1px ${props => props.theme.stoneGrey};
+  outline: none;
+  color: inherit;
+  &:focus {
+    transition: .2s linear;
+    border-color: ${props => props.theme.skyBlue};;
+    box-shadow: 0 0 3px ${props => props.theme.skyBlue};
+  }
+  font-size: 100%;
+  text-overflow: ellipsis;
+`;
+
+const IconWrapper = styled('div')`
+  position: absolute;
+  ${props => props.iconPosition}: 10px;
+  font-size: 0;
+  line-height: 1;
+`;
+
+const renderIcon = (iconPosition, color) => (
+  <IconWrapper iconPosition={iconPosition}>
+    {searchIcon(color, '21px')}
+  </IconWrapper>
+);
+
+const Searchbar = ({ iconPosition, placeholder, theme, onChange, value, ...props }) => (
+  <Wrapper>
+    {renderIcon(iconPosition, theme.skyBlue)}
+    <Input
+      type="text"
+      placeholder={placeholder}
+      iconPosition={iconPosition}
+      onChange={onChange}
+      value={value}
+      {...props}
+    />
+  </Wrapper>
 );
 
 Searchbar.defaultProps = {
   placeholder: 'Search',
   onChange: () => null,
-  iconPosition: 'right'
+  iconPosition: 'right',
+  value: ''
 };
 
 Searchbar.propTypes = {
   placeholder: PropTypes.string,
   onChange: PropTypes.func,
-  iconPosition: PropTypes.oneOf(['left', 'right'])
+  iconPosition: PropTypes.oneOf(['left', 'right']),
+  hideIcon: PropTypes.bool,
+  theme: PropTypes.object,
+  value: PropTypes.string
 };
 
-export default Searchbar;
+export default withTheme(Searchbar);
---
+++ src/components/Searchbar/Searchbar.md
@@ -0,0 +1,29 @@
+Default:
+```jsx
+  <Searchbar />
+```
+
+Icon on the left
+```jsx
+  <Searchbar iconPosition="left" />
+```
+
+Without an icon
+```jsx
+  <Searchbar hideIcon />
+```
+
+Custom placeholder:
+```jsx
+  <Searchbar placeholder="Search for dresses" />
+```
+
+Filled value:
+```jsx
+  <div><Searchbar value="Floral dress" /></div>
+```
+
+Truncated text:
+```jsx
+  <div style={{ width: '150px'}}><Searchbar iconPosition="left" value="You can't see all of me, but I'm there" /></div>
+```
