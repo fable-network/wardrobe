edcdae6e2e39a3de72c882472be83ccdc618b2f5
Kirill Kriuchkov <kirill.kriuchkov@fashiontrade.com>
2018-10-17 15:15:13 +0200

    add(*): very basic typography

---
+++ src/theme/typography.js
@@ -0,0 +1,10 @@
+export const FONT_SIZE_BASE = 16;
+
+const typography = {
+  fontSizeBase: '1rem',
+  lineHeightBase: '1.5',
+  fontSizeSmall: `${14 / 16}rem`,
+  lineHeightSmall: '1',
+};
+
+export default typography;
