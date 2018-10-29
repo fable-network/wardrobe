a6f25ab5012538a9d8ee8062a9c59cad4bcffdd5
Kirill Kriuchkov <kirill.kriuchkov@fashiontrade.com>
2018-10-29 17:06:25 +0100

    change(theme/decorations): unify theme dictionaries

--- src/theme/decorations.js
+++ src/theme/decorations.js
@@ -1,6 +1,6 @@
 const decorations = {
-  shadow: 'box-shadow: 0 2px 4px 0 rgba(49, 50, 51, 0.2)',
-  noShadow: 'box-shadow: none;',
+  shadow: '0 2px 4px 0 rgba(49, 50, 51, 0.2)',
+  noShadow: 'none',
 };
 
 export default decorations;
