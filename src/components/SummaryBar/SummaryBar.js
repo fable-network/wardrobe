97cdeb3b4f9dd2d4eddfbc5177a110d7e5e21f59
Kirill Kriuchkov <kirill.kriuchkov@fashiontrade.com>
2019-03-04 18:41:47 +0100

    WIP SummaryBar

--- src/components/SummaryBar/SummaryBar.js
+++ src/components/SummaryBar/SummaryBar.js
@@ -3,34 +3,77 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Stack from '../../layout/Stack';
 
-const StackHorizontal = props => <Stack direction="horizontal" size="large" {...props} />;
+const SummaryBarSection = React.Fragment;
 
-const Wrapper = styled(StackHorizontal)`
-  padding: 1rem 0;
-  max-width: 100%;
-  > * {
-    flex-grow: 0;
-    flex-shrink: 1;
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+
+  ${p => p.theme.desktop_up`
+    flex-direction: row;
+  `};
+`;
+
+const createArray = count => {
+  const array = new Array(count);
+  for (let i = 0; i < count; i += 1) array[i] = i;
+  return array;
+};
+
+const Content = styled(({ cols, ...otherProps }) => <div {...otherProps} />)`
+  display: grid;
+  grid-template-columns: ${p =>
+    createArray(p.cols)
+      .map(() => 'auto')
+      .join(' ')};
+  > :not(:last-child) {
+    margin-right: ${p => p.theme.stackMarginLarge};
   }
 `;
 
-const Buttons = styled(StackHorizontal)`
-  margin-left: auto;
-  padding-left: ${p => p.theme.stackMarginLarge};
+const Buttons = styled(Stack)`
+  justify-content: center;
+  margin-top: ${p => p.theme.stackMarginLarge};
+  ${p => p.theme.desktop_up`
+    justify-content: flex-start;
+    margin-top: 0;
+    margin-left: auto;
+    padding-left: ${p.theme.stackMarginLarge};
+  `};
 `;
 
-const SummaryBar = ({ buttons, children, ...otherProps }) => (
-  <Wrapper {...otherProps}>
-    {children}
-    {Boolean(buttons) && <Buttons>{buttons}</Buttons>}
-  </Wrapper>
-);
+const childrenMaxColsReducer = (max, item) =>
+  item.type === SummaryBarSection && item.props.children && item.props.children.length > max
+    ? item.props.children.length
+    : max;
 
-SummaryBar.defaultProps = {};
+const mapChildren = children => children;
+//   React.Children.map(children, child => React.cloneElement(child, { key: this.props.innerKey }));
+
+const SummaryBar = ({ buttons, children, ...otherProps }) => {
+  const maxCols = Array.isArray(children) ? children.reduce(childrenMaxColsReducer, 1) : 1;
+  return (
+    <Wrapper {...otherProps}>
+      <Content cols={maxCols}>{mapChildren(children, maxCols)}</Content>
+      {Boolean(buttons) && (
+        <Buttons direction="horizontal" size="large">
+          {buttons}
+        </Buttons>
+      )}
+    </Wrapper>
+  );
+};
+
+SummaryBar.defaultProps = {
+  children: [],
+};
 
 SummaryBar.propTypes = {
   children: PropTypes.node,
   buttons: PropTypes.node,
 };
 
+SummaryBar.Section = SummaryBarSection;
+
+/** @component */
 export default SummaryBar;
--- src/components/SummaryBar/SummaryBar.md
+++ src/components/SummaryBar/SummaryBar.md
@@ -17,32 +17,38 @@ const Button = require('../Button').default;
 </SummaryBar>;
 ```
 
-With more stuff:
+With more stuff and splitting into multiple rows (on mobile and tablet) using `SummaryBar.Section`:
 
 ```jsx
 const DisplayValue = require('../DisplayValue').default;
 const Button = require('../Button').default;
-const Ruler = require('../Ruler').default;
 <SummaryBar
   buttons={
     <React.Fragment>
-      <Button><nobr>Clear cart</nobr></Button>
+      <Button>
+        <nobr>Clear cart</nobr>
+      </Button>
       <Button appearance="primary">Checkout</Button>
     </React.Fragment>
   }
 >
-  <DisplayValue label="Styles">3</DisplayValue>
-  <DisplayValue label="Pre-packs">1</DisplayValue>
-  <DisplayValue label="Pieces">10</DisplayValue>
-  <Ruler direction="vertical" />
-  <DisplayValue label="Total RRP">EUR 20,456.00</DisplayValue>
-  <DisplayValue label="Total WSP">
-    EUR <span style={{ textDecoration: 'line-through' }}>15,982.00</span>{' '}
-    <span style={{ color: 'red' }}>13,500.55</span>
-  </DisplayValue>
-  <DisplayValue label="Markup">2.3</DisplayValue>
-  <DisplayValue label="Discount">
-    <span style={{color: 'red'}}>10%</span>
-   </DisplayValue>
+  <SummaryBar.Section>
+    <DisplayValue label="Styles">3</DisplayValue>
+    <DisplayValue label="Pre-packs">1</DisplayValue>
+    <DisplayValue label="Pieces">10</DisplayValue>
+  </SummaryBar.Section>
+  <SummaryBar.Section>
+    <DisplayValue label="Total RRP">EUR 20,456.00</DisplayValue>
+    <DisplayValue label="Total WSP">
+      EUR <span style={{ textDecoration: 'line-through' }}>15,982.00</span>{' '}
+      <span style={{ color: 'red' }}>13,500.55</span>
+    </DisplayValue>
+  </SummaryBar.Section>
+  <SummaryBar.Section>
+    <DisplayValue label="Markup">2.3</DisplayValue>
+    <DisplayValue label="Discount">
+      <span style={{ color: 'red' }}>10%</span>
+    </DisplayValue>
+  </SummaryBar.Section>
 </SummaryBar>;
 ```
